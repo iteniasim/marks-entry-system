20b1877669ee2de660e5a4ca19878cb8075d8839
iteniasim <iteniasim.hulkstar@gmail.com>
2022-02-14 10:32:20 +0545

    enable source maps & disable success notification

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,7 +18,9 @@ mix.js('resources/js/app.js', 'public/js')
         require('tailwindcss'),
         require('autoprefixer'),
     ])
-    .webpackConfig(require('./webpack.config'));
+    .webpackConfig(require('./webpack.config'))
+    .sourceMaps()
+    .disableSuccessNotifications();
 
 if (mix.inProduction()) {
     mix.version();
