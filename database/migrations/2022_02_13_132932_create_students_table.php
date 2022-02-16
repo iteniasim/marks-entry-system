e753555d6c47ec51a24a46bbcf7d45eaa61da57c
iteniasim <iteniasim.hulkstar@gmail.com>
2022-02-16 23:16:14 +0545

    create migration for student exam subject and grades

--- database/migrations/2022_02_13_132932_create_students_table.php
+++ database/migrations/2022_02_13_132932_create_students_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Grade;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,10 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('roll_no');
+            $table->foreignIdFor(Grade::class)->constrained();
             $table->timestamps();
         });
     }
