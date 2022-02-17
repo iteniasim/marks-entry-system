fd6a8ba34fba6b06c642c5d77be0b43a84086f53
iteniasim <iteniasim.hulkstar@gmail.com>
2022-02-17 10:53:28 +0545

    change slot name from action to actions

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { ref } from 'vue';
-import BreezeApplicationLogo from '@/Components/ApplicationLogo.vue';
-import BreezeDropdown from '@/Components/Dropdown.vue';
-import BreezeDropdownLink from '@/Components/DropdownLink.vue';
-import BreezeNavLink from '@/Components/NavLink.vue';
-import BreezeResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link } from '@inertiajs/inertia-vue3';
+import { ref } from 'vue'
+import BreezeApplicationLogo from '@/Components/ApplicationLogo.vue'
+import BreezeDropdown from '@/Components/Dropdown.vue'
+import BreezeDropdownLink from '@/Components/DropdownLink.vue'
+import BreezeNavLink from '@/Components/NavLink.vue'
+import BreezeResponsiveNavLink from '@/Components/ResponsiveNavLink.vue'
+import { Link } from '@inertiajs/inertia-vue3'
 
-const showingNavigationDropdown = ref(false);
+const showingNavigationDropdown = ref(false)
 </script>
 
 <template>
@@ -114,7 +114,7 @@ const showingNavigationDropdown = ref(false);
                             <slot name="header" />
                         </div>
                         <div>
-                            <slot name="action" />
+                            <slot name="actions" />
                         </div>
                     </div>
                 </div>
