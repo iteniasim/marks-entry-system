61793c70acb8ce00b035afd258537fea9a36baee
iteniasim <iteniasim.hulkstar@gmail.com>
2022-02-20 00:45:04 +0545

    remove create and edit url getters from student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,21 +10,6 @@ class Student extends Model
     use HasFactory;
 
     protected $guarded = [];
-
-    protected $appends = [
-        'create_url',
-        'edit_url',
-    ];
-
-    public function createUrl(): Attribute
-    {
-        return Attribute::get(fn() => route('students.create'));
-    }
-
-    public function editUrl(): Attribute
-    {
-        return Attribute::get(fn() => route('students.edit', $this->id));
-    }
 
     public function grade()
     {
