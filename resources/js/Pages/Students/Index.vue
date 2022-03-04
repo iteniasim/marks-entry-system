a799d61461cca29a81738d794e9b0f1fb2a10710
iteniasim <iteniasim.hulkstar@gmail.com>
2022-03-04 20:03:57 +0545

    fix sutdent index layout

--- resources/js/Pages/Students/Index.vue
+++ resources/js/Pages/Students/Index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
-import { Head, Link } from '@inertiajs/inertia-vue3'
+import { Head } from '@inertiajs/inertia-vue3'
+import { TButton } from '@variantjs/vue'
 
 const pageTitle = 'Students'
 
@@ -18,38 +19,43 @@ const props = defineProps({
         </template>
 
         <template #actions>
-            <Link :href="route('students.create')" class="btn btn-primary btn-sm">
+            <t-button :href="route('students.create')">
                 Add Student
-            </Link>
+            </t-button>
         </template>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <table class="table min-w-full table-zebra">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Roll</th>
-                            <th>Grade</th>
-                            <th>Actions</th>
+                <table class="min-w-full border border-gray-200 divide-y divide-gray-100 shadow-sm">
+                    <thead class>
+                        <tr class>
+                            <th class="px-3 py-2 font-semibold text-left bg-gray-100 border-b">Name</th>
+                            <th class="px-3 py-2 font-semibold text-left bg-gray-100 border-b">Roll</th>
+                            <th class="px-3 py-2 font-semibold text-left bg-gray-100 border-b">Grade</th>
+                            <th class="px-3 py-2 font-semibold text-left bg-gray-100 border-b">Actions</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody class="bg-white divide-y divide-gray-100">
                         <tr
-                            class="hover"
                             v-for="(student, index) in props.students"
                             :key="`student-${index}`"
                         >
-                            <td>{{ student.name }}</td>
-                            <td>{{ student.roll_no }}</td>
-                            <td>{{ student.grade.name }}</td>
-                            <td>
-                                <Link class="btn btn-link" :href="route('students.edit', student.id)">
+                            <td class="px-3 py-2 whitespace-no-wrap">
+                                {{ student.name }}
+                            </td>
+                            <td class="px-3 py-2 whitespace-no-wrap">
+                                {{ student.roll_no }}
+                            </td>
+                            <td class="px-3 py-2 whitespace-no-wrap">
+                                {{ student.grade.name }}
+                            </td>
+                            <td class="flex items-center gap-4">
+                                <t-button :href="route('students.edit', student.id)">
                                     Edit
-                                </Link>
-                                <Link class="btn btn-link" method="delete" as="button" type="button" :href="route('students.destroy', student.id)">
+                                </t-button>
+                                <t-button method="delete" as="button" type="button" :href="route('students.destroy', student.id)">
                                     Delete
-                                </Link>
+                                </t-button>
                             </td>
                         </tr>
                     </tbody>
