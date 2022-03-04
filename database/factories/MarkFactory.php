0ab94fab902b9bff00236a56b9ebd703e99467d5
iteniasim <iteniasim.hulkstar@gmail.com>
2022-03-04 15:43:59 +0545

    commit unused files first

---
+++ database/factories/MarkFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Mark>
+ */
+class MarkFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            // 'year' => Carbon::now()->year(),
+            // 'student_id' => 1,
+            // 'subject_id' => 5,
+            // 'exam_id' => 5,
+            // 'grade_id' => 5,
+            // 'full_marks' => 4,
+            // 'pass_marks' => 4,
+            // 'obtained_marks' => 4,
+        ];
+    }
+}
