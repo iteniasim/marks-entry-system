648e9e880e2fb886741f4dd08e5c4f8bcaa13c4a
iteniasim <iteniasim.hulkstar@gmail.com>
2022-06-22 13:31:22 +0545

    add filters in marks index page

--- resources/js/Pages/Marks/Index.vue
+++ resources/js/Pages/Marks/Index.vue
@@ -1,14 +1,46 @@
 <script setup>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
 import { Head, Link } from '@inertiajs/inertia-vue3'
-import { TButton } from '@variantjs/vue'
+import { TButton, TInput, TRichSelect } from '@variantjs/vue'
 import PaginationComponent from '@/Components/PaginationComponent.vue'
+import axios from 'axios'
+import { onMounted, ref, watch } from 'vue'
+import debounce from 'lodash/debounce'
 
 const pageTitle = 'Marks'
 
 const props = defineProps({
     students: Object,
 })
+
+const studentList = ref([])
+
+const grades = ref([])
+
+const fectchGrades = () => {
+    axios.get(route('grades.index', { withoutPagination: true })).then(res => {
+        grades.value = res.data.grades
+    })
+}
+
+onMounted(() => {
+    fectchGrades()
+    studentList.value = props.students
+})
+
+const search = ref(null)
+const searchGrade = ref(null)
+
+const fetchSearchResults = () => {
+    axios.get(route('marks.index', { search: search.value, grade: searchGrade.value })).then(res => {
+        studentList.value = res.data.students
+        search.value = res.data.search ? res.data.search : null
+        searchGrade.value = res.data.grade ? parseInt(res.data.grade) : null
+    })
+}
+
+watch(search, debounce(fetchSearchResults, 2000))
+watch(searchGrade, debounce(fetchSearchResults, 2000))
 </script>
 
 <template>
@@ -29,7 +61,25 @@ const props = defineProps({
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div v-if="props.students.total">
+                <div v-if="studentList.total">
+                    <div class="grid grid-cols-2 gap-12 mb-8">
+                        <div>
+                            <TInput
+                                name="search"
+                                v-model="search"
+                                placeholder="Search Subject"
+                            />
+                        </div>
+                        <div>
+                            <t-rich-select
+                                :options="grades"
+                                v-model="searchGrade"
+                                placeholder="Select Grade"
+                                value-attribute="id"
+                                text-attribute="name"
+                            />
+                        </div>
+                    </div>
                     <table class="min-w-full border border-gray-200 divide-y divide-gray-100 shadow-sm">
                         <thead class>
                             <tr class>
@@ -40,7 +90,7 @@ const props = defineProps({
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-100">
                             <tr
-                                v-for="(student, index) in props.students.data"
+                                v-for="(student, index) in studentList.data"
                                 :key="`student-${index}`"
                             >
                                 <td class="px-3 py-2 whitespace-no-wrap">
@@ -57,7 +107,7 @@ const props = defineProps({
                             </tr>
                         </tbody>
                     </table>
-                    <pagination-component :data="props.students" />
+                    <pagination-component :data="studentList" />
                 </div>
 
                 <div v-else>
